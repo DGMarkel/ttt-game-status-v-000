54c5f53a9878360b320153d809524794a7d109d4
David Markel <davidgmarkel@gmail.com>
2018-03-06 04:04:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,6 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |combination|
     board[combination[0]] == board[combination[1]] && board[combination[0]] == board[combination[2]]
-    && position_taken?(board, combination[1]) 
+    && position_taken?(board, combination[0]) 
   end
 end
