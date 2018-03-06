3eff4b9f643bbf8840ac26e16b642df97fb6746a
David Markel <davidgmarkel@gmail.com>
2018-03-06 04:39:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,11 +30,6 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
-    true
-  elsif !won?(board) && !full?(board)
-    false
-  elsif won?(board)
-    true
+  !won?(board) && full?(board)
   end
 end
